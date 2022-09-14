eb29221e52bad818e9db27181f84eadf55dd5dc6
john ghosh <dev.ui@snaphunt.com>
2022-09-14 21:39:39 +0530

    css change

--- lit/projectCard.js
+++ lit/projectCard.js
@@ -23,7 +23,7 @@ class ProjectCard extends LitElement{
     render(){
         return html`
             <div class="card">
-                <img src="${this.img}" width="100px" height="100px"/>
+                <img src="${this.img}" width="200px" height="200px"/>
             
                 <div class="container">
                     <h3> <b> ${this.name} </b> </h3>
